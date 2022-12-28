67af69c11644fd7afe885dc59079fab0143a0dcd
Gastón Farrando <97707561+gfarrando@users.noreply.github.com>
2022-12-28 14:25:56 -0300

    News

---
+++ src/context/NoticiasProvider.jsx
@@ -0,0 +1,60 @@
+import { useState, useEffect, createContext } from "react";
+import axios from "axios";
+const NoticiasContext = createContext();
+
+const NoticiasProvider = ({ children }) => {
+  const [categoria, setCategoria] = useState("general");
+  const [noticias, setNoticias] = useState([]);
+  const [pagina, setPagina] = useState(1);
+  const [totalNoticias, setTotalNoticias] = useState(0);
+
+  useEffect(() => {
+    const consultarApi = async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=ar&category=${categoria}&apiKey=${
+        import.meta.env.VITE_API_KEY
+      }`;
+      const { data } = await axios(url);
+      setNoticias(data.articles);
+      setTotalNoticias(data.totalResults);
+      setPagina(1);
+    };
+    consultarApi();
+  }, [categoria]);
+
+  useEffect(() => {
+    const consultarApi = async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=ar&page=${pagina}&category=${categoria}&apiKey=${
+        import.meta.env.VITE_API_KEY
+      }`;
+      const { data } = await axios(url);
+      setNoticias(data.articles);
+      setTotalNoticias(data.totalResults);
+    };
+    consultarApi();
+  }, [pagina]);
+
+  const handleChangeCategoria = (e) => {
+    setCategoria(e.target.value);
+  };
+  const handleChangePagination = (e, valor) => {
+    setPagina(valor);
+  };
+  return (
+    <NoticiasContext.Provider
+      value={{
+        categoria,
+        handleChangeCategoria,
+        noticias,
+        totalNoticias,
+        handleChangePagination,
+        pagina,
+      }}
+    >
+      {children}
+    </NoticiasContext.Provider>
+  );
+};
+
+export { NoticiasProvider };
+
+export default NoticiasContext;
